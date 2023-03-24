f718f6b96b3661009bdf24f53f93e7dda2b4acb8
joseb <josebs525@gmail.com>
2023-03-24 14:26:47 -0600

    practica26.2

--- particle.js
+++ particle.js
@@ -1,8 +1,8 @@
 var accessToken = "ef863e1c826c1e21e18eb4b443721f0b01f6e894";
 var deviceID = "220021001947393035313138"
 var url = "https://api.particle.io/v1/devices/" + deviceID + "/gradosC";
-var url2 = "https://api.particle.io/v1/devices/" + deviceID + "/humedad";
-var url3 = "https://api.particle.io/v1/devices/" + deviceID + "/gradosF";
+//var url2 = "https://api.particle.io/v1/devices/" + deviceID + "/humedad";
+//var url3 = "https://api.particle.io/v1/devices/" + deviceID + "/gradosF";
 function callback(data, status){
 	if (status == "success") {
 		temp = parseFloat(data.result);
