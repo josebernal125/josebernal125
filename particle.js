febd56f18728566d88b78fc5af4f83549edc63f9
joseb <josebs525@gmail.com>
2023-03-29 18:11:28 -0600

    practica31

--- particle.js
+++ particle.js
@@ -1,14 +1,16 @@
 var accessToken = "ef863e1c826c1e21e18eb4b443721f0b01f6e894";
 var deviceID = "220021001947393035313138"
-var parOn = "on"
-var parOff = "off"
-var url = "https://api.particle.io/v1/devices/" + deviceID + "/led+-d+access_token="+ accessToken + "+-d+params=";
+var url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
+document.getElementById("myImage").src = pic;
 
+function switchOn()
+{
+	$.post(url, {params: "on", access_token: accessToken });
+	pic = "focoTurnOn.png"
+}  
 
-function getReadingOn(){
-  $.get(url+parOn);
-}
-
-function getReadingOff(){
-  $.get(url+parOff);
-}
+function switchOff()
+{
+	$.post(url, {params: "off", access_token: accessToken });
+	pic = "focoTurnOff.png"
+}  
