138e9981792c9c33a833290779f79501ac2e44c1
joseb <josebs525@gmail.com>
2023-03-31 04:41:58 -0600

    practica32

--- particle.js
+++ particle.js
@@ -1,18 +1,33 @@
 var accessToken = "ef863e1c826c1e21e18eb4b443721f0b01f6e894";
 var deviceID = "220021001947393035313138"
 var url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
-document.getElementById("myImage").src = pic;
+var datos_json;
 
 function switchOn()
 {
-	$.post(url, {params: "on", access_token: accessToken });
-	pic = "focoTurnOn.png"
-	document.getElementById("myImage").src = pic;
+	$.post(url, {params: "on", access_token: accessToken })
+	.done(switchIMG);
 }  
 
 function switchOff()
 {
-	$.post(url, {params: "off", access_token: accessToken });
-	pic = "focoTurnOff.png"
-	document.getElementById("myImage").src = pic;
-}  
+	$.post(url, {params: "off", access_token: accessToken })
+	.done(switchIMG);
+}
+
+function switchIMG(data) {
+    var rv = data.return_value;
+    $("#return_value").text(rv);
+    if(rv == 1){
+        pic = "focoTurnOn.png"
+		$("#status").text("Connected");
+    }
+    else if(rv == 0){
+        pic = "focoTurnOff.png"
+		$("#status").text("Connected");
+    } else {
+        pic = "connectionlost.png"
+		$("#status").text("Disconnected");
+    }
+    document.getElementById("myImage").src = pic;
+}
