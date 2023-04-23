408c7fcb7b79014705e6b2de9ab2f5984aa18c2a
joseb <josebs525@gmail.com>
2023-04-22 21:41:34 -0600

    practica39

--- particle4.js
+++ particle4.js
@@ -1,32 +1,27 @@
 var accessToken = "ef863e1c826c1e21e18eb4b443721f0b01f6e894";
 var deviceID = "220021001947393035313138"
-var url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
+var url = "https://api.particle.io/v1/devices/" + deviceID + "/servo";
+
 
 function switchOn()
 {
-	$.post(url, {params: "255,0,0", access_token: accessToken })
-	//.done(switchIMG);
-}  
-
-function switchOff()
-{
-	$.post(url, {params: "0,0,0", access_token: accessToken })
-	//.done(switchIMG);
+	$.post(url, {params: "on", access_token: accessToken })
+	.done(switchIMG);
 }
 
 function switchIMG(data) {
     var rv = data.return_value;
     $("#return_value").text(rv);
     if(rv == 1){
-        pic = "focoTurnOn.png"
-		$("#status").text("Connected");
+        
+		$("#status").text("Peces Alimentados").css("display", "block");
+        setTimeout(desaparecer, 3000);
     }
-    else if(rv == 0){
-        pic = "focoTurnOff.png"
-		$("#status").text("Connected");
-    } else {
-        pic = "connectionlost.png"
+    else {
+        
 		$("#status").text("Disconnected");
     }
-    document.getElementById("myImage").src = pic;
+}
+function desaparecer(){
+    $("#status").css("display", "none"); // ocultar la etiqueta después de 3 segundos
 }
